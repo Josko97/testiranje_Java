cf5d059ea219c21f4380f9d0c8a996ad6911800e
Josko <joskobarbir1988@gmail.com>
2024-12-18 22:54:04 +0100

    Add files via upload

---
+++ zadatak_2/src/main/java/com/testiranje/java/josko/converter/implementation/ArticleDataConverter.java
@@ -0,0 +1,17 @@
+package com.testiranje.java.josko.converter.implementation;
+
+import com.testiranje.java.josko.converter.FileConverter;
+import com.testiranje.java.josko.model.ArticleData;
+
+public class ArticleDataConverter implements FileConverter{
+
+	@Override
+	public ArticleData fetchData(String line) {
+		
+		
+		
+		return null;
+	}
+	
+
+}
